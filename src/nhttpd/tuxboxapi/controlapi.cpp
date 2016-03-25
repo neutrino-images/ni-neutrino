49762cac5c14076a9e6dbada7135dbf87c4fc00d
TangoCash <eric@loxat.de>
2016-03-25 08:46:25 +0100

    add option to reload channels via webif without saving first

--- src/nhttpd/tuxboxapi/controlapi.cpp
+++ src/nhttpd/tuxboxapi/controlapi.cpp
@@ -2182,7 +2182,8 @@ void CControlAPI::ReloadPluginsCGI(CyhookHandler *hh)
 
 void CControlAPI::ReloadChannelsCGI(CyhookHandler *hh)
 {
-	CServiceManager::getInstance()->SaveServices(true, true);
+	if(hh->ParamList["hardreload"].empty())
+		CServiceManager::getInstance()->SaveServices(true, true);
 	NeutrinoAPI->Zapit->reinitChannels();
 	CNeutrinoApp::getInstance()->SDTreloadChannels = false;
 	hh->SendOk();
