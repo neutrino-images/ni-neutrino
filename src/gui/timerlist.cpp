a03b7bcb1e8b75b13e64eba080146e6f33d1a6a8
vanhofen <vanhofen@gmx.de>
2017-05-23 23:11:34 +0200

    - timerlist: don't use rc_info with remotetimers; that doesn't work

--- src/gui/timerlist.cpp
+++ src/gui/timerlist.cpp
@@ -1042,7 +1042,7 @@ int CTimerList::show()
 			CTimerd::responseGetTimer* timer=&timerlist[selected];
 			if (timer!=NULL)
 			{
-				if (timer->eventType == CTimerd::TIMER_RECORD || timer->eventType == CTimerd::TIMER_REMOTEBOX || timer->eventType == CTimerd::TIMER_ZAPTO)
+				if (timer->eventType == CTimerd::TIMER_RECORD || timer->eventType == CTimerd::TIMER_ZAPTO) //NI
 				{
 					hide();
 					if (timer->epgID != 0)
@@ -1397,7 +1397,7 @@ void CTimerList::paintFoot()
 		if (timer != NULL)
 		{
 		//replace info button with dummy if timer is not type REC or ZAP
-			if (timer->eventType == CTimerd::TIMER_RECORD || timer->eventType == CTimerd::TIMER_REMOTEBOX || timer->eventType == CTimerd::TIMER_ZAPTO)
+			if (timer->eventType == CTimerd::TIMER_RECORD || timer->eventType == CTimerd::TIMER_ZAPTO) //NI
 				TimerListButtons[4].button = NEUTRINO_ICON_BUTTON_INFO_SMALL;
 			else
 				TimerListButtons[4].button = NEUTRINO_ICON_BUTTON_DUMMY_SMALL;
