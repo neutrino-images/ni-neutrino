eaabd0ca485b554bf79820e4ece99731a2d1242d
vanhofen <vanhofen@gmx.de>
2017-06-18 23:00:12 +0200

    - audioplayer: add url to metadata

--- src/gui/audioplayer.cpp
+++ src/gui/audioplayer.cpp
@@ -1024,7 +1024,6 @@ bool CAudioPlayerGui::shufflePlaylist(void)
 void CAudioPlayerGui::addUrl2Playlist(const char *url, const char *name, const time_t bitrate)
 {
 	CAudiofileExt mp3(url, CFile::STREAM_AUDIO);
-	//tmp = tmp.substr(0,tmp.length()-4);	//remove .url
 	//printf("[addUrl2Playlist], name = %s, url = %s\n", name, url);
 	if (name != NULL)
 	{
@@ -1039,6 +1038,8 @@ void CAudioPlayerGui::addUrl2Playlist(const char *url, const char *name, const t
 		mp3.MetaData.total_time = bitrate;
 	else
 		mp3.MetaData.total_time = 0;
+
+	mp3.MetaData.url = url;
 
 	if (url[0] != '#')
 		addToPlaylist(mp3);
