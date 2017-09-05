ed68d9dfc6ff4aec5ff197bd8a7d2031adf12fee
vanhofen <vanhofen@gmx.de>
2017-09-05 08:49:02 +0200

    - nhttpd: remove unused mod_testhook

--- src/nhttpd/yhttpd.cpp
+++ src/nhttpd/yhttpd.cpp
@@ -250,11 +250,6 @@ void Cyhttpd::hooks_attach() {
 	CyhookHandler::attach(auth);
 #endif
 
-#ifdef Y_CONFIG_USE_TESTHOOK
-	testhook = new CTesthook();
-	CyhookHandler::attach(testhook);
-#endif
-
 #ifdef CONFIG_SYSTEM_TUXBOX
 	NeutrinoAPI = new CNeutrinoAPI();
 	CyhookHandler::attach(NeutrinoAPI->NeutrinoYParser);
@@ -286,11 +281,6 @@ void Cyhttpd::hooks_detach() {
 #ifdef Y_CONFIG_USE_AUTHHOOK
 	CyhookHandler::detach(auth);
 	delete auth;
-#endif
-
-#ifdef Y_CONFIG_USE_TESTHOOK
-	CyhookHandler::detach(testhook);
-	delete testhook;
 #endif
 
 #ifdef CONFIG_SYSTEM_TUXBOX
