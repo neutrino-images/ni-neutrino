157fabea513cb8734b845f2cf9c02fd2f3870029
vanhofen <vanhofen@gmx.de>
2017-11-01 15:17:52 +0100

    - rename can_set_display_brightness => display_can_set_brightness

--- lib/hardware/coolstream/hardware_caps.h
+++ lib/hardware/coolstream/hardware_caps.h
@@ -33,7 +33,7 @@ typedef struct hw_caps
 	display_type_t display_type;
 	int display_xres;	/* x resolution or chars per line */
 	int display_yres;
-	int can_set_display_brightness;
+	int display_can_set_brightness;
 	char boxvendor[64];
 	char boxname[64];
 	char boxarch[64];
