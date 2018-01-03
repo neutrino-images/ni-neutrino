9d8bcb18bd6258d0bf6e67fea3e444e98b2e88f0
vanhofen <vanhofen@gmx.de>
2018-01-03 14:05:43 +0100

    - neutrino: remove unused __EXPERIMENTAL_CODE__ stuff

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -195,12 +195,6 @@ cCpuFreqManager * cpuFreq;
 void stop_lcd4l_support(void); //NI lcd4l-support
 void stop_daemons(bool stopall = true, bool for_flash = false);
 void stop_video(void);
-// uncomment if you want to have a "test" menue entry  (rasc)
-
-//#define __EXPERIMENTAL_CODE__
-#ifdef __EXPERIMENTAL_CODE__
-#include "gui/ch_mosaic.h"
-#endif
 
 CAudioSetupNotifier	* audioSetupNotifier;
 CBouquetList   * bouquetList; // current list
