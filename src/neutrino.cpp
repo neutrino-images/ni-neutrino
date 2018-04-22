35fa18c3d753b69d73dd59d9525b60065fca3fd2
vanhofen <vanhofen@gmx.de>
2018-04-22 22:48:04 +0200

    - neutrino: use WEBTVDIR as default for g_settings.livestreamScriptPath
    The usage of WEBTVDIR_VAR was not a good idea because our delivered xml's
    are located in WEBTVDIR.
    In summery the location doesn't really matter, because our code in
    movieplayer.cpp will find the script in several places.

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -1050,7 +1050,7 @@ int CNeutrinoApp::loadSetup(const char * fname)
 	g_settings.infoClockSeconds = configfile.getInt32("infoClockSeconds", 0); //NI
 
 	g_settings.livestreamResolution = configfile.getInt32("livestreamResolution", 1920);
-	g_settings.livestreamScriptPath = configfile.getString("livestreamScriptPath", WEBTVDIR_VAR);
+	g_settings.livestreamScriptPath = configfile.getString("livestreamScriptPath", WEBTVDIR);
 
 	g_settings.version_pseudo = configfile.getString("version_pseudo", "19700101000000");
 
