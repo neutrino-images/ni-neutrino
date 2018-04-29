1af35f36c5775dc153a9907c04583f69cbf01e87
Thilo Graf <dbt@novatux.de>
2018-04-29 03:13:08 +0200

    imdb-support: renamed imdb->omdb
    Imdb is not really used directly, that is a bit misleading

--- src/system/settings.h
+++ src/system/settings.h
@@ -814,12 +814,14 @@ struct SNeutrinoSettings
 	int   movieplayer_repeat_on;
 	int movieplayer_bisection_jump; //NI
 	int movieplayer_display_playtime;
+
+	//online services
 	std::string youtube_dev_id;
 	int youtube_enabled;
 	std::string tmdb_api_key;
 	int tmdb_enabled;
-	std::string imdb_api_key;
-	int imdb_enabled;
+	std::string omdb_api_key;
+	int omdb_enabled;
 	std::string shoutcast_dev_id;
 	int shoutcast_enabled;
 
