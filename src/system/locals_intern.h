337a2ba37e115e898c93f5104b4f981e0f92f4e4
vanhofen <vanhofen@gmx.de>
2018-07-25 00:44:37 +0200

    - record: differentiate between timeshift and record in AskToStop() function

--- src/system/locals_intern.h
+++ src/system/locals_intern.h
@@ -2592,6 +2592,7 @@ const char * locale_real_names[] =
 	"shoutcast.dev_id",
 	"shoutcast.enabled",
 	"shutdown.recording_query",
+	"shutdown.timeshift_query",
 	"shutdowntimer.announce",
 	"sleeptimerbox.announce",
 	"sleeptimerbox.hint1",
