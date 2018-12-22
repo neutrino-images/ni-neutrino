6fd6de31ad093b72f3f0c5c711fe0c9a5be3b4dd
vanhofen <vanhofen@gmx.de>
2018-12-22 09:24:03 +0100

    - simplify more includes of compatibility headers

--- src/gui/osd_setup.cpp
+++ src/gui/osd_setup.cpp
@@ -67,12 +67,7 @@
 #include <system/setting_helpers.h>
 #include "cs_api.h"
 
-#if HAVE_COOL_HARDWARE
-#include <video_cs.h>
-#endif
-#if USE_STB_HAL
-#include <video_hal.h>
-#endif
+#include <hardware/video.h>
 
 extern CRemoteControl * g_RemoteControl;
 
