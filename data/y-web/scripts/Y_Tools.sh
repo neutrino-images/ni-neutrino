6e52c6fea9acd6f8d2211c99e643497fd1a31a19
vanhofen <vanhofen@gmx.de>
2019-01-29 22:14:12 +0100

    - yWeb: remove type from Y_version.txt

--- data/y-web/scripts/Y_Tools.sh
+++ data/y-web/scripts/Y_Tools.sh
@@ -581,7 +581,7 @@ case "$1" in
 	fbshot)					shift 1; do_fbshot $* ;;
 	fbshot_clear)			do_fbshot_clear ;;
 	screenshot_clear)		do_screenshot_clear ;;
-	get_update_version)		wget -O /tmp/version.txt "http://raw.github.com/tuxbox-neutrino/gui-neutrino/master/src/nhttpd/web/Y_Version.txt" ;;
+	get_update_version)		wget -O /tmp/version.txt "https://bitbucket.org/neutrino-images/ni-neutrino-hd/raw/HEAD/data/y-web/Y_Version.txt" ;;
 	settings_backup_restore)	shift 1; do_settings_backup_restore $* ;;
 	exec_cmd)				shift 1; $* ;;
 	automount_list)			shift 1; do_automount_list $* ;;
