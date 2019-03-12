0462547303a406a58dfb24d991cf26ebf97a3f58
Thilo Graf <dbt@novatux.de>
2019-03-12 22:00:11 +0100

    /widget/menue.h/cpp: add missing signal OnAfterChangeOption()

--- src/gui/widget/menue.cpp
+++ src/gui/widget/menue.cpp
@@ -2237,6 +2237,7 @@ int CMenuOptionStringChooser::exec(CMenuTarget* parent)
 	else if(observ) {
 		wantsRepaint = observ->changeNotify(name, (void *)(optionValuePtr ? optionValuePtr->c_str() : ""));
 	}
+	OnAfterChangeOption();
 	if (wantsRepaint)
 		ret = menu_return::RETURN_REPAINT;
 
