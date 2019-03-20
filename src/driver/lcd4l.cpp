7e775d0c5fb9f02314a6b93583e4055c47324488
vanhofen <vanhofen@gmx.de>
2019-03-20 23:51:49 +0100

    - lcd4l: write weather_city file

--- src/driver/lcd4l.cpp
+++ src/driver/lcd4l.cpp
@@ -104,6 +104,7 @@ extern CPictureViewer *g_PicViewer;
 #define FCOLOR2			LCD_DATADIR "fcolor2"
 #define PBCOLOR			LCD_DATADIR "pbcolor"
 
+#define WEATHER_CITY		LCD_DATADIR "weather_city"
 #define WEATHER_TEMP		LCD_DATADIR "weather_temp"
 #define WEATHER_ICON		LCD_DATADIR "weather_icon"
 
@@ -253,6 +254,7 @@ void CLCD4l::Init()
 	m_Start		= "00:00";
 	m_End		= "00:00";
 
+	m_wcity		= "";
 	m_wtemp		= "";
 	m_wicon		= "";
 
@@ -1018,6 +1020,13 @@ void CLCD4l::ParseInfo(uint64_t parseID, bool newID, bool firstRun)
 
 	if (g_settings.weather_enabled && CWeather::getInstance()->checkUpdate(firstRun))
 	{
+		std::string wcity = CWeather::getInstance()->getCity();
+		if (m_wcity.compare(wcity))
+		{
+			WriteFile(WEATHER_CITY, wcity);
+			m_wcity = wcity;
+		}
+
 		int forecast = 1; // days for forecast
 
 		std::string wtemp = CWeather::getInstance()->getActTemp();
@@ -1038,7 +1047,6 @@ void CLCD4l::ParseInfo(uint64_t parseID, bool newID, bool firstRun)
 			m_wicon = wicon;
 		}
 	}
-
 }
 
 /* ----------------------------------------------------------------- */
--- src/driver/lcd4l.h
+++ src/driver/lcd4l.h
@@ -126,6 +126,7 @@ class CLCD4l
 		std::string	m_fcolor2;
 		std::string	m_pbcolor;
 
+		std::string	m_wcity;
 		std::string	m_wtemp;
 		std::string	m_wicon;
 };
