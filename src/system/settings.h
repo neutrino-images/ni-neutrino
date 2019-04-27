710009728ebd6682b61661110142ea49c276f038
Jacek Jendrzej <overx300@gmail.com>
2019-04-28 00:32:34 +0200

    add option to adzap: zap on timer activation

--- src/system/settings.h
+++ src/system/settings.h
@@ -879,6 +879,8 @@ struct SNeutrinoSettings
 	//adzap
 	int adzap_zapBackPeriod;
 	int adzap_writeData;
+	int adzap_zapOnActivation;
+	enum{ADZAP_ZAP_OFF,ADZAP_ZAP_TO_LAST,ADZAP_ZAP_TO_STRAT};
 
 	int	power_standby;
 	int	hdd_sleep;
