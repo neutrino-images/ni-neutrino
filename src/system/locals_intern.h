6605fa9e023ef6acd8bf31f6863f89596358f5d8
Jacek Jendrzej <overx300@gmail.com>
2019-06-02 12:13:34 +0200

    YT: add download dir menu

--- src/system/locals_intern.h
+++ src/system/locals_intern.h
@@ -2111,6 +2111,7 @@ const char * locale_real_names[] =
 	"moviebrowser.yt_clear",
 	"moviebrowser.yt_completed",
 	"moviebrowser.yt_concurrent_connections",
+	"moviebrowser.yt_dl_dir",
 	"moviebrowser.yt_error",
 	"moviebrowser.yt_failed",
 	"moviebrowser.yt_history",
