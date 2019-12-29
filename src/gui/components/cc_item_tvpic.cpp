53456b82330f4691b9867f83d66062aaa14e40ed
Thilo Graf <dbt@novatux.de>
2019-12-29 23:06:12 +0100

    Images: add logo file

--- src/gui/components/cc_item_tvpic.cpp
+++ src/gui/components/cc_item_tvpic.cpp
@@ -52,8 +52,7 @@ CComponentsPIP::CComponentsPIP(	const int x_pos, const int y_pos, const int perc
 	//CComponentsPIP
 	screen_w = frameBuffer->getScreenWidth(true);
 	screen_h = frameBuffer->getScreenHeight(true);
-	pic_name = ICONSDIR;
-	pic_name += "/start.jpg";
+	pic_name = frameBuffer->getIconPath("logo", "jpg");
 
 	//CComponents
 	x 		= x_pos;
