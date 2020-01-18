bfab1febdb95279b135c0993c8c52127841dd28e
BPanther <bpanther_ts@hotmail.com>
2020-01-18 23:06:12 +0100

    move ci save pin code to each slot menu (but still only valid for slot 0)

--- src/system/settings.h
+++ src/system/settings.h
@@ -291,12 +291,13 @@ struct SNeutrinoSettings
 	int ci_rpr[4];
 #endif
 	int ci_ignore_messages[4];
-	int ci_save_pincode;	// TODO: for each slot
+	int ci_save_pincode[4];
+	std::string ci_pincode[4];
 	int ci_check_live;
 	int ci_tuner;
 	int ci_rec_zapto; //NI
 	int ci_mode; //NI
-	std::string ci_pincode;	// TODO: for each slot
+
 	int radiotext_enable;
 	int webradio_xml_auto;
 	int webtv_xml_auto;
