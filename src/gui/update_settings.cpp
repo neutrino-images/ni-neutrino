981f5d6f1f169c689e0631a7dd91b0a5c15cec04
Thilo Graf <dbt@novatux.de>
2021-04-10 22:14:39 +0200

    neutrino.cpp: enable check for package updates with new setting

--- src/gui/update_settings.cpp
+++ src/gui/update_settings.cpp
@@ -39,6 +39,7 @@
 #include <neutrino_menue.h>
 #include <gui/filebrowser.h>
 #include <gui/update_check.h>
+#include <gui/opkg_manager.h>
 #include <gui/update_ext.h>
 #include <gui/update_settings.h>
 #include <gui/widget/icons.h>
@@ -144,7 +145,8 @@ int CUpdateSettings::initMenu()
 	OnOffNotifier->addItem(apply_kernel);
 #endif
 
-	CMenuOptionChooser *autocheck = new CMenuOptionChooser(LOCALE_FLASHUPDATE_AUTOCHECK, &g_settings.softupdate_autocheck, OPTIONS_OFF0_ON1_OPTIONS, OPTIONS_OFF0_ON1_OPTION_COUNT, true, this);
+	CMenuOptionChooser *autocheck = new CMenuOptionChooser(LOCALE_FLASHUPDATE_AUTOCHECK, &g_settings.softupdate_autocheck, OPTIONS_OFF0_ON1_OPTIONS, OPTIONS_OFF0_ON1_OPTION_COUNT, true, OnOffNotifier);
+	CMenuOptionChooser *package_autocheck = new CMenuOptionChooser("Package update check", &g_settings.softupdate_autocheck_packages, OPTIONS_OFF0_ON1_OPTIONS, OPTIONS_OFF0_ON1_OPTION_COUNT, true, OnOffNotifier);
 //	apply_settings->setHint("", LOCALE_MENU_HINT_XXX);
 
 	w_upsettings.addItem(fw_update_dir);
@@ -158,7 +160,8 @@ int CUpdateSettings::initMenu()
 #endif
 #endif
 	w_upsettings.addItem(autocheck);
-
+	if (COPKGManager::hasOpkgSupport())
+		w_upsettings.addItem(package_autocheck);
 #if 0
 	w_upsettings.addItem(apply_kernel);
 #endif
