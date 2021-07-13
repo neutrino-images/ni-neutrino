c58d74674be29ee3612b5c7559a16191c5a06190
vanhofen <vanhofen@gmx.de>
2021-07-13 23:37:43 +0200

    - rcinput: add RC_back

--- src/driver/rcinput.cpp
+++ src/driver/rcinput.cpp
@@ -1589,6 +1589,8 @@ const char * CRCInput::getSpecialKeyName(const unsigned int key)
 				return "standby";
 			case RC_home:
 				return "home";
+			case RC_back:
+				return "back";
 			case RC_setup:
 				return "setup";
 			case RC_red:
