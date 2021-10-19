6161eeef9b2e79db5f4bf57d44a6d4e83ba77487
vanhofen <vanhofen@gmx.de>
2021-10-19 22:35:24 +0200

    - introduce configurable ZAPITDIR

--- src/system/setting_helpers.cpp
+++ src/system/setting_helpers.cpp
@@ -660,7 +660,7 @@ int CDataResetNotifier::exec(CMenuTarget* /*parent*/, const std::string& actionK
 	}
 
 	if(delete_all) {
-		my_system(3, "/bin/sh", "-c", "rm -f " CONFIGDIR "/zapit/*.conf");
+		my_system(3, "/bin/sh", "-c", "rm -f " ZAPITDIR "/*.conf");
 		CServiceManager::getInstance()->SatelliteList().clear();
 		CZapit::getInstance()->LoadSettings();
 		CZapit::getInstance()->GetConfig(zapitCfg);
@@ -686,7 +686,7 @@ int CDataResetNotifier::exec(CMenuTarget* /*parent*/, const std::string& actionK
 		CFrameBuffer::getInstance()->Clear();
 	}
 	if(delete_chan) {
-		my_system(3, "/bin/sh", "-c", "rm -f " CONFIGDIR "/zapit/*.xml");
+		my_system(3, "/bin/sh", "-c", "rm -f " ZAPITDIR "/*.xml");
 		g_Zapit->reinitChannels();
 	}
 	if (delete_removed) {
