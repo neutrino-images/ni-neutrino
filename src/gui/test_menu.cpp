4ea1c1e5f6c61e00713c80d9017a4e3134785dcc
vanhofen <vanhofen@gmx.de>
2022-09-27 23:58:23 +0200

    - hourglass: add defaults for x/y position

--- src/gui/test_menu.cpp
+++ src/gui/test_menu.cpp
@@ -1238,7 +1238,7 @@ int CTestMenu::exec(CMenuTarget *parent, const std::string &actionKey)
 	}
 	else if (actionKey == "hourglass")
 	{
-		CHourGlass hg(20, 20);
+		CHourGlass hg;
 		hg.paint();
 		sleep(10);
 		hg.hide();
