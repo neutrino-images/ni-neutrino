193c5aa41d76f4a9908225de48843b003df338c3
vanhofen <vanhofen@gmx.de>
2022-10-05 21:48:04 +0200

    - streamts: add missing brackets

--- src/driver/streamts.cpp
+++ src/driver/streamts.cpp
@@ -196,8 +196,10 @@ void CStreamInstance::run()
 	dmx->Start(true);
 
 	if (is_e2_stream)
+	{
 		if (g_settings.streaming_decryptmode)
 			CCamManager::getInstance()->Start(channel_id, CCamManager::STREAM);
+	}
 	else
 		CCamManager::getInstance()->Start(channel_id, CCamManager::STREAM);
 
@@ -216,8 +218,10 @@ void CStreamInstance::run()
 	}
 
 	if (is_e2_stream)
+	{
 		if (g_settings.streaming_decryptmode)
 			CCamManager::getInstance()->Stop(channel_id, CCamManager::STREAM);
+	}
 	else
 		CCamManager::getInstance()->Stop(channel_id, CCamManager::STREAM);
 
@@ -528,8 +532,8 @@ void CStreamManager::PreparePids(CZapitChannel *channel, stream_pids_t &pids, bo
 	{
 		for(casys_pids_iterator_t it = channel->capids.begin(); it != channel->capids.end(); ++it)
 		{
-		  pids.insert((*it)); //all ECM Pids
-		  printf("CStreamManager::PreparePids: capid 0x%04x \n", (*it));
+			pids.insert((*it)); //all ECM Pids
+			printf("CStreamManager::PreparePids: capid 0x%04x \n", (*it));
 		}
 	}
 
