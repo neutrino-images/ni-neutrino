a29d505ce8de5e29cc2b5513adfe10f95ef941d3
vanhofen <vanhofen@gmx.de>
2023-03-03 22:27:35 +0100

    - yWeb: minor resort in Y_Info_Menue

--- src/nhttpd/tuxboxapi/neutrinoyparser.h
+++ src/nhttpd/tuxboxapi/neutrinoyparser.h
@@ -52,7 +52,6 @@ private:
 	std::string func_get_webtv_list(CyhookHandler *hh, std::string para);
 	std::string func_get_webradio_list(CyhookHandler *hh, std::string para);
 	std::string func_get_xmltv_list(CyhookHandler *hh, std::string para);
-	//NI y-funcs
 	std::string func_infoicons_set_values(CyhookHandler *hh, std::string para);
 	std::string func_netfs_set_values(CyhookHandler *hh, std::string para);
 	std::string func_netfs_read_config(CyhookHandler *hh, std::string para);
