839367bd49b552f1c3415c6a9349a342f5fa2409
vanhofen <vanhofen@gmx.de>
2025-12-30 20:33:48 +0100

    - frontend: remove unused functions

--- src/zapit/src/frontend.cpp
+++ src/zapit/src/frontend.cpp
@@ -742,14 +742,6 @@ fe_status_t CFrontend::getStatus(void) const
 	return (fe_status_t) (event.status & FE_HAS_LOCK);
 }
 
-#if 0
-//never used
-FrontendParameters CFrontend::getFrontend(void) const
-{
-	return currentTransponder.feparams;
-}
-#endif
-
 uint32_t CFrontend::getBitErrorRate(void) const
 {
 	uint32_t ber = 0;
@@ -774,17 +766,6 @@ uint16_t CFrontend::getSignalNoiseRatio(void) const
 	fop(ioctl, FE_READ_SNR, &snr);
 	return snr;
 }
-
-#if 0
-//never used
-uint32_t CFrontend::getUncorrectedBlocks(void) const
-{
-	uint32_t blocks = 0;
-	fop(ioctl, FE_READ_UNCORRECTED_BLOCKS, &blocks);
-
-	return blocks;
-}
-#endif
 
 struct dvb_frontend_event CFrontend::getEvent(void)
 {
