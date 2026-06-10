bd70a5eb33787a25df903bdc0f5c2367deaf1d07
vanhofen <vanhofen@gmx.de>
2026-06-10 22:43:26 +0200

    - data/scripts: minor output changes in ntp-helper.sh

--- data/scripts/ntp-helper.sh
+++ data/scripts/ntp-helper.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 chronyc=$(which chronyc)
-conf=/etc/chrony.conf
 ntpserver=$1
 
 if [ -z $chronyc -o -z $ntpserver ]; then
 	exit 1
 fi
 
+conf=/etc/chrony.conf
 pool=$(grep ^pool $conf | cut -d" " -f2)
 
 if [ "$pool" == "$ntpserver" ]; then
-	echo "${0##*/}: Synchronize time with $pool"
+	printf "${0##*/}: Synchronize time with $pool ... "
 	$chronyc makestep
 else
-	echo "${0##*/}: Replacing $pool with $ntpserver in $conf"
+	echo "${0##*/}: Replace pool $pool with $ntpserver in $conf"
 	sed -i -e "s|^pool .*|pool $ntpserver iburst|" $conf
-	echo "${0##*/}: Restarting chronyd"
+	echo "${0##*/}: Restart chrony"
 	service chrony restart
 fi
