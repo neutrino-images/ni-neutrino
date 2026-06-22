eadcee10e854f64ed7fdf3424f7d67e1ffb80f54
Thilo Graf <dbt@novatux.de>
2026-06-23 00:50:08 +0200

    fix (system): parse builder image version stamps safely
    Extend CFlashVersionInfo to recognise local builder timestamp metadata.
    Keep support for the legacy encoded image version stamp.
    Invalid strings now stay on the Unknown fallback.
    
    Do not rely on unchecked numeric conversion.
    
    Ref: https://github.com/neutrino-images/ni-neutrino/commit/d361757feea15febf62cbdff9e1bd3a73a118b76

--- src/system/flashtool.cpp
+++ src/system/flashtool.cpp
@@ -29,6 +29,7 @@
 #include <eitd/sectionsd.h>
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <libgen.h>
@@ -46,6 +47,28 @@
 #ifdef ENABLE_LCD4LINUX
 #include "driver/lcd4l.h"
 #endif
+
+static bool flashVersionHasDigits(const std::string &value, size_t pos, size_t len)
+{
+	if (pos + len > value.size())
+		return false;
+
+	for (size_t i = pos; i < pos + len; i++)
+	{
+		if (value[i] < '0' || value[i] > '9')
+			return false;
+	}
+
+	return true;
+}
+
+static int flashVersionNumber(const std::string &value, size_t pos, size_t len)
+{
+	if (!flashVersionHasDigits(value, pos, len))
+		return 0;
+
+	return atoi(value.substr(pos, len).c_str());
+}
 
 CFlashTool::CFlashTool()
 {
@@ -499,96 +522,121 @@ void CFlashTool::reboot()
 
 CFlashVersionInfo::CFlashVersionInfo(const std::string &_versionString)
 {
-	// SVVVYYYYMMTTHHMM    -- formatsting SemVer (16 chars)
-	// SVVVVVVYYYYMMTTHHMM -- formatsting CalVer (19 chars)
 	std::string versionString = _versionString;
+	size_t typePos = std::string::npos;
+	size_t versionPos = std::string::npos;
+	size_t versionLen = 0;
+	size_t yearPos = std::string::npos;
+	size_t monthPos = std::string::npos;
+	size_t dayPos = std::string::npos;
+	size_t hourPos = std::string::npos;
+	size_t minutePos = std::string::npos;
 
-	bool calver = false;
+	snapshot = '?';
+	version = 0;
+	strcpy(vstring, "0.00");
+	strcpy(date, "00.00.0000");
+	strcpy(time, "00:00");
+	datetime = 0;
 
-	// just to make sure the string is long enough for the following code
-	// trailing chars don't matter -- will just be ignored
-	if (versionString.size() < 16)
+	// Calendar-version stamp with explicit type: SYYYYMMYYYYMMDDHHMM.
+	if (versionString.size() >= 19 && flashVersionHasDigits(versionString, 1, 18))
 	{
-		versionString.append(16, '0');
-		// we're assuming semantic version
+		typePos = 0;
+		versionPos = 1;
+		versionLen = 6;
+		yearPos = 7;
+		monthPos = 11;
+		dayPos = 13;
+		hourPos = 15;
+		minutePos = 17;
 	}
-	else if (versionString.size() > 16)
+	// Legacy: SBBBYYYYMMDDHHMM.
+	else if (versionString.size() >= 16 && flashVersionHasDigits(versionString, 1, 15))
 	{
-		versionString.append(19, '0');
-		// we're assuming calendar version
-		calver = true;
+		typePos = 0;
+		versionPos = 1;
+		versionLen = 3;
+		yearPos = 4;
+		monthPos = 8;
+		dayPos = 10;
+		hourPos = 12;
+		minutePos = 14;
+	}
+	// Builder timestamp without encoded type: YYYYMMDDHHMM[SS].
+	else if (versionString.size() >= 12 && flashVersionHasDigits(versionString, 0, 12))
+	{
+		snapshot = '9';
+		versionPos = 0;
+		versionLen = 6;
+		yearPos = 0;
+		monthPos = 4;
+		dayPos = 6;
+		hourPos = 8;
+		minutePos = 10;
+	}
+	else
+	{
+		return;
 	}
 
-	// start positions in versionString
-	int S = 0; // snapshot/type
-	int V = 1; // version
-	// release date
-	int Y = calver ? 7 : 4; // year
-	int M = Y + 4; // month
-	int D = M + 2; // day
-	int h = D + 2; // hour
-	int m = h + 2; // minute
-
-	// recover type
-	snapshot = versionString[S];
+	if (typePos != std::string::npos)
+		snapshot = versionString[typePos];
 
 	// human readable version
-	if (calver)
+	if (versionLen == 6)
 	{
-		vstring[0] = versionString[V];
-		vstring[1] = versionString[V + 1];
-		vstring[2] = versionString[V + 2];
-		vstring[3] = versionString[V + 3];
+		vstring[0] = versionString[versionPos];
+		vstring[1] = versionString[versionPos + 1];
+		vstring[2] = versionString[versionPos + 2];
+		vstring[3] = versionString[versionPos + 3];
 		vstring[4] = '.';
-		vstring[5] = versionString[V + 4];
-		vstring[6] = versionString[V + 5];
+		vstring[5] = versionString[versionPos + 4];
+		vstring[6] = versionString[versionPos + 5];
 		vstring[7] = 0;
 	}
-	else // SemVer
+	else
 	{
-		vstring[0] = versionString[V];
+		vstring[0] = versionString[versionPos];
 		vstring[1] = '.';
-		vstring[2] = versionString[V + 1];
-		vstring[3] = versionString[V + 2];
+		vstring[2] = versionString[versionPos + 1];
+		vstring[3] = versionString[versionPos + 2];
 		vstring[4] = 0;
-		vstring[5] = 0;
-		vstring[6] = 0;
-		vstring[7] = 0;
 	}
 
-	version = std::stoi(versionString.substr(1, calver ? 6 : 3));
+	version = flashVersionNumber(versionString, versionPos, versionLen);
 
 	// recover date
 	struct tm tt;
 	memset(&tt, 0, sizeof(tt));
-
-	date[0] = versionString[D];
-	date[1] = versionString[D + 1];
+	date[0] = versionString[dayPos];
+	date[1] = versionString[dayPos + 1];
 	date[2] = '.';
 	tt.tm_mday = atoi(&date[0]);
 
-	date[3] = versionString[M];
-	date[4] = versionString[M + 1];
+	date[3] = versionString[monthPos];
+	date[4] = versionString[monthPos + 1];
 	date[5] = '.';
 	tt.tm_mon = atoi(&date[3]) - 1;
 
-	date[6] = versionString[Y];
-	date[7] = versionString[Y + 1];
-	date[8] = versionString[Y + 2];
-	date[9] = versionString[Y + 3];
+	date[6] = versionString[yearPos];
+	date[7] = versionString[yearPos + 1];
+	date[8] = versionString[yearPos + 2];
+	date[9] = versionString[yearPos + 3];
 	date[10] = 0;
 	tt.tm_year = atoi(&date[6]) - 1900;
 
 	// recover time stamp
-	time[0] = versionString[h];
-	time[1] = versionString[h + 1];
+	time[0] = versionString[hourPos];
+	time[1] = versionString[hourPos + 1];
 	time[2] = ':';
 	tt.tm_hour = atoi(&time[0]);
 
-	time[3] = versionString[m];
-	time[4] = versionString[m + 1];
+	time[3] = versionString[minutePos];
+	time[4] = versionString[minutePos + 1];
 	time[5] = 0;
 	tt.tm_min = atoi(&time[3]);
+	tt.tm_isdst = -1;
 
 	datetime = mktime(&tt);
 }
